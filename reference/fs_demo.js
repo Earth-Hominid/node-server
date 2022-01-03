2ffe390a42163e5f8d88d8a8d96c28ef50e43e2a
C <earthhominid@gmail.com>
2022-01-03 09:17:40 -0300

    os methods

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -26,3 +26,12 @@ fs.writeFile(
     );
   }
 );
+
+fs.readFile(
+  path.join(__dirname, '/test', 'hello.txt'),
+  'utf-8',
+  (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  }
+);
