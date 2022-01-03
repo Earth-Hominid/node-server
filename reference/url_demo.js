bd30f9ef311ba14b5d3d1f1e2d726eb13fc0146b
C <earthhominid@gmail.com>
2022-01-03 09:37:50 -0300

    url demo

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -1,6 +1,8 @@
 const url = require('url');
 
-const myUrl = new URL('http://mywebsite.com/hello.html?id=100&status=active');
+const myUrl = new URL(
+  'http://mywebsite.com:8000/hello.html?id=100&status=active'
+);
 
 // Serialized URL
 console.log(myUrl.href);
@@ -9,3 +11,25 @@ console.log(myUrl.toString());
 
 // Host (root) domain
 console.log(myUrl.host);
+
+// Hostname (Notice hostname doesn't get the port but host does)
+console.log(myUrl.hostname);
+
+// Pathname
+console.log(myUrl.pathname);
+
+// Serialized query
+console.log(myUrl.search);
+
+// Search Params object
+
+console.log(myUrl.searchParams);
+
+// Add param
+myUrl.searchParams.append('abc', '123');
+console.log(myUrl.searchParams);
+
+// Loop through params
+console.log(
+  myUrl.searchParams.forEach((value, name) => console.log(`${name}: ${value}`))
+);
